77345b2f980cd9c039c6eb432239e23393aa11f0
Shravan Sathiyanarayanan <bl.en.u4cse23053@bl.students.amrita.edu>
2025-04-06 22:08:29 +0530

    correction commit

--- memez.go
+++ memez.go
@@ -1,68 +1,111 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-	"math/rand"
-	"time"
-	"github.com/gorilla/mux"
+    "encoding/json"
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
+    "math/rand"
+    "net/http"
+    "os"
+    "strconv"
+    "time"
 )
 
-// Function to get all files in a specific directory (images and GIFs)
-func getMemesFromDirectory(dir string) ([]string, error) {
-	var files []string
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		// Check if file is an image or GIF
-		if !info.IsDir() && (filepath.Ext(path) == ".jpg" || filepath.Ext(path) == ".jpeg" || filepath.Ext(path) == ".png" || filepath.Ext(path) == ".gif") {
-			files = append(files, path)
-		}
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-	return files, nil
+type Meme struct {
+    ID    int    `json:"id"`
+    Title string `json:"title"`
+    URL   string `json:"url"`
 }
 
-// API handler to serve a random meme
-func getRandomMeme(w http.ResponseWriter, r *http.Request) {
-	// Directory where your memes (images and GIFs) are stored
-	memeDirectory := "./memes"
-	files, err := getMemesFromDirectory(memeDirectory)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error reading memes directory: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	// If there are no memes found, return an error message
-	if len(files) == 0 {
-		http.Error(w, "No memes found!", http.StatusNotFound)
-		return
-	}
-
-	// Select a random meme from the list of files
-	rand.Seed(time.Now().UnixNano())
-	randomMeme := files[rand.Intn(len(files))]
-
-	// Serve the random meme as a response
-	http.ServeFile(w, r, randomMeme)
-}
+var memes []Meme
 
 func main() {
-	r := mux.NewRouter()
+    if err := loadMemesFromFile("memes.json"); err != nil {
+        panic("Failed to load memes: " + err.Error())
+    }
 
-	// Route to get a random meme
-	r.HandleFunc("/random-meme", getRandomMeme).Methods("GET")
+    r := gin.Default()
+    r.Use(cors.Default()) // allows access from websites
 
-	// Start the server
-	fmt.Println("Starting server on :8080...")
-	log.Fatal(http.ListenAndServe(":8080", r))
+    r.GET("/memes", getAllMemes)
+    r.GET("/memes/random", getRandomMeme)
+    r.GET("/memes/:id", getMemeByID)
+    r.POST("/memes", addMeme)
+
+    r.Run(":8080") // runs on http://localhost:8080
+}
+
+func getAllMemes(c *gin.Context) {
+    c.JSON(http.StatusOK, memes)
+}
+
+func getMemeByID(c *gin.Context) {
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+        return
+    }
+
+    for _, meme := range memes {
+        if meme.ID == id {
+            c.JSON(http.StatusOK, meme)
+            return
+        }
+    }
+
+    c.JSON(http.StatusNotFound, gin.H{"error": "Meme not found"})
+}
+
+func getRandomMeme(c *gin.Context) {
+    rand.Seed(time.Now().UnixNano())
+    if len(memes) == 0 {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "No memes available"})
+        return
+    }
+    meme := memes[rand.Intn(len(memes))]
+    c.JSON(http.StatusOK, meme)
+}
+
+func addMeme(c *gin.Context) {
+    var newMeme Meme
+    if err := c.ShouldBindJSON(&newMeme); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+        return
+    }
+
+    newMeme.ID = getNextID()
+    memes = append(memes, newMeme)
+
+    if err := saveMemesToFile("memes.json"); err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not save meme"})
+        return
+    }
+
+    c.JSON(http.StatusOK, newMeme)
+}
+
+func loadMemesFromFile(filename string) error {
+    data, err := os.ReadFile(filename)
+    if err != nil {
+        return err
+    }
+    return json.Unmarshal(data, &memes)
+}
+
+func saveMemesToFile(filename string) error {
+    data, err := json.MarshalIndent(memes, "", "  ")
+    if err != nil {
+        return err
+    }
+    return os.WriteFile(filename, data, 0644)
+}
+
+func getNextID() int {
+    max := 0
+    for _, meme := range memes {
+        if meme.ID > max {
+            max = meme.ID
+        }
+    }
+    return max + 1
 }
